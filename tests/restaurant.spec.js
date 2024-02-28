5b3189dcdb2eb7ff5dca2efc2a5b549ca3a3ddbc
b166erobot <bernard0freitas@outlook.com>
2024-02-28 00:21:06 -0300

    Ainda fazendo o requisito 10

--- tests/restaurant.spec.js
+++ tests/restaurant.spec.js
@@ -1,8 +1,14 @@
 const createMenu = require('../src/restaurant');
- 
+
 describe('10 - Implemente a função `createMenu`, bem como seus casos de teste', () => {
   it('Verifica se a função `createMenu` tem o comportamento esperado', () => {
-    fail('Teste vazio!');
     // Escreva todos os testes aqui.
+    expect(createMenu()).toHaveProperty('fetchMenu');
+    expect(typeof createMenu().fetchMenu).toBe('function');
+    let result = createMenu({food: {}, drinks: {}}).fetchMenu();
+    expect(result).toHaveProperty('food');
+    expect(result).toHaveProperty('drinks');
+    expect(result).toEqual({food: {}, drinks: {}});
+    expect(result).toHaveProperty('consumption', []);
   });
 });
